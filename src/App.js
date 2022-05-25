8958524791c3f845bc4291c43ad8fb9bd2043dca
David White <webdigga42@gmail.com>
2022-05-25 20:14:55 +0100

    Adding dynamic routing and search params

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
+import { 
+	Routes,
+	Route,
+	Link,
+	Outlet,
+	NavLink,
+	useParams,
+	useNavigate,
+	useSearchParams
+} from 'react-router-dom';
+
+import * as React from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const navigate = useNavigate();
+
+	const [users, setUsers] = React.useState([
+		{ id: '1', fullName: 'Robin Wieruch' },
+		{ id: '2', fullName: 'Sarah Finnley' }
+	]);
+
+	const handleRemoveUser = ( userId) => {
+		setUsers((state) => state.filter(( user ) => user.id !== userId));
+
+		navigate('/users');
+	}
+
+	return (
+		<>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="home" element={<Home />} />
+					<Route path="users" element={<Users users={users} />}>
+						<Route 
+							path=":userId"
+							element={<User onRemoveUser={handleRemoveUser} />}>
+						</Route>
+					</Route>
+					<Route path="*" element={<NoMatch />} />
+				</Route>
+			</Routes>
+		</>
+	);
+};
+
+const Users = ({ users }) => {
+	const [searchParams, setSearchParams] = useSearchParams();
+  
+	const searchTerm = searchParams.get('name') || '';
+  
+	const handleSearch = (event) => {
+	  const name = event.target.value;
+  
+	  if (name) {
+		setSearchParams({ name: event.target.value });
+	  } else {
+		setSearchParams({});
+	  }
+	};
+  
+	return (
+	  <>
+		<h2>Users</h2>
+  
+		<input
+		  type="text"
+		  value={searchTerm}
+		  onChange={handleSearch}
+		/>
+  
+		<ul>
+		  {users
+			.filter((user) =>
+			  user.fullName
+				.toLowerCase()
+				.includes(searchTerm.toLocaleLowerCase())
+			)
+			.map((user) => (
+			  <li key={user.id}>
+				<Link to={user.id}>{user.fullName}</Link>
+			  </li>
+			))}
+		</ul>
+  
+		<Outlet />
+	  </>
+	);
+  };
+
+function User( {onRemoveUser} ) {
+	const {userId} = useParams();
+
+	return (
+		<>
+			<h2>User: {userId}</h2>
+
+			<button type="button" onClick={() => onRemoveUser(userId)}>
+				Remove
+			</button>
+
+			<Link to="/users">Back to users</Link>
+		</>
+	)
+}
+
+function NoMatch() {
+	return (<p>There's no page here: 404!</p>);
+}
+
+function Home() {
+	return (
+		<>
+			<h2>Home</h2>
+		</>
+	);
+};
+
+function Layout() {
+	const style = ({ isActive }) => ({
+		fontWeight: isActive ? 'bold' : 'normal'
+	});
+
+	return (
+		<>
+			<h1>React router</h1>
+
+			<nav
+				style={{
+					borderBottom: 'solid 1px',
+					paddingBottom: '1rem'
+				}}
+			>
+				<NavLink to="/home" style={style}>Home</NavLink>
+				<NavLink to="/users" style={style}>Users</NavLink>
+			</nav>
+
+			<main style={{ padding: '1rem 0'}}>
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default App;
